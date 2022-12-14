e13c8638206c61847fbcd201972c62ae881582d5
Arihin <arihin.slava@yandex.ru>
2022-12-14 10:41:05 +0300

    Домашнее задание 2. 1 задание.

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Human human = new Human();
-        human.name = "Максим";
-        human.yearOfBirth = 1988;
-        human.town = " Минск";
-        human.jobTitle = "бренд - менеджер";
+        Human human = new Human("Максим","Минск",1988,"бренд-менеджер");
         human.greeting();
 
-        Human human1 = new Human();
-        human1.name = "Аня";
-        human1.yearOfBirth = 1993;
-        human1.town = " Москва ";
-        human1.jobTitle = " методист образовательных программ";
+        Human human1 = new Human("Аня","Москва",1993,"методист образовательных программ");
         human1.greeting();
 
-        Human human2 = new Human();
-        human2.name = "Катя";
-        human2.yearOfBirth = 1992;
-        human2.town = "Калининград";
-        human2.jobTitle = "продакт-менеджер";
+        Human human2 = new Human("Катя","Калининград",1992,"продакт-менеджер");
         human2.greeting();
 
-        Human human3 = new Human();
-        human3.name = "Артём";
-        human3.yearOfBirth = 1995;
-        human3.town = " Москва ";
-        human3.jobTitle = " директор по развитию бизнеса ";
+        Human human3 = new Human("Артём","Москва",1995,"Директор по развитию бизнеса");
         human3.greeting();
     }
 }
