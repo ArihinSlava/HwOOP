aa1990e3c707b9560f02d7e809da1cb2b70cf834
Arihin <arihin.slava@yandex.ru>
2022-12-14 10:04:33 +0300

    Домашнее задание 1. 1/2 задание.

--- src/Human.java
+++ src/Human.java
@@ -8,4 +8,5 @@ public class Human {
     void greeting() {
         System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
     }
+
 }
